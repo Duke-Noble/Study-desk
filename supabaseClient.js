3c0bfebbe6c91197cf1c4f0a56a0aae42aeaab7e
Duke-Noble <nobleduke588@gmail.com>
2026-05-04 20:55:58 +0000

    Delete supabaseClient.js cos it's in the src folder

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Replace the strings below with your actual Supabase Project details
-const supabaseUrl = 'https://wlipykzhlysdkwtqfcbl.supabase.co'
-const supabaseAnonKey = 'sb_publishable_j4y_edxMfOj8Is31ZnmPnw_HmqiY9uc'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
